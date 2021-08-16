601d4136db66815f4d7a4329e03c71150a51719e
montoya <ricardo.montoya@rcxit.com.br>
2021-08-16 09:01:03 -0300

    add header in all routes

--- src/config/layout/index.jsx
+++ src/config/layout/index.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
+import HashLinkObserver from "react-hash-link";
+import { useLocation } from "react-router-dom";
+
+import Header from "../../components/Header";
 
 export default function Layout({ children }) {
-  return <div>{children}</div>;
+  const { pathname } = useLocation();
+
+  return (
+    <div className="bg-blueGray-600 h-full">
+      <Header isTransparent={pathname === "/" ? true : false} />
+      {children}
+      <HashLinkObserver />
+    </div>
+  );
 }
 
 Layout.propTypes = {
