fa3bc116eb49d88a9b0da9e6f6396a721045c493
montoya <ricardo.montoya@rcxit.com.br>
2021-08-16 09:04:39 -0300

    add movie & result routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,13 +3,20 @@ import { Redirect, Switch } from "react-router-dom";
 
 import Route from "./RouteWrapper";
 import Home from "../pages/Home";
+import Result from "../pages/Result";
+import View from "../pages/View";
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Redirect to="/" />
-    </Switch>
+    <>
+      <Switch>
+        <Route path="/result" component={Result} />
+        <Route path="/movie/:id" component={View} />
+        <Route path="/" component={Home} />
+
+        <Redirect to="/" />
+      </Switch>
+    </>
   );
 };
 export default Routes;
