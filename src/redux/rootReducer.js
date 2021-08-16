0366b05f369bbb644f091370b36a87f0f23aa867
montoya <ricardo.montoya@rcxit.com.br>
2021-08-16 09:04:09 -0300

    add view result & trending reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,7 +1,11 @@
 import { combineReducers } from "redux";
 
-import { homeSlice } from "../pages/Home/redux/slice";
+import { resultSlice } from "../pages/Result/redux/slice";
+import { trendingSlice } from "../pages/Trending/redux/slice";
+import { viewSlice } from "../pages/View/redux/slice";
 
 export const rootReducer = combineReducers({
-  home: homeSlice.reducer,
+  trending: trendingSlice.reducer,
+  result: resultSlice.reducer,
+  view: viewSlice.reducer,
 });
