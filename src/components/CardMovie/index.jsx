180e198b0b16d83aaeb55a6416ece8446bb15c47
montoya <ricardo.montoya@rcxit.com.br>
2021-10-01 10:16:22 -0300

    refactor: change layout cards

--- src/components/CardMovie/index.jsx
+++ src/components/CardMovie/index.jsx
@@ -1,48 +1,65 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { CardMovieInfo } from "./styles";
+import ReactSVG from "react-inlinesvg";
 
 import formattedDate from "../../utils/dateFormat";
 
 const CardMovie = ({ movie }) => {
   const history = useHistory();
 
+  let movieOverview = movie.overview;
+  if (movieOverview.length > 150) {
+    movieOverview = movieOverview.substring(0, 150) + "...";
+  }
+
   return (
-    <div className="flex flex-col justify-between w-64 xs:h-full hover:shadow-2xl hover:scale-105 transition-all transform duration-500 max-w-xs bg-primary-50 rounded-md shadow-lg overflow-hidden">
+    <CardMovieInfo>
       <div>
         {movie.backdrop_path ? (
           <img
-            src={`https://image.tmdb.org/t/p/w300${movie.backdrop_path}`}
+            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
             alt=""
+            onClick={() => history.push(`/movie/${movie.id}`)}
           />
         ) : (
           <img src="/assets/images/empty.png" alt="" />
         )}
       </div>
-      <div className="p-3 space-y-3">
-        <h3 className="text-gray-700 font-semibold text-md">{movie.title}</h3>
-        <p className="text-sm text-gray-900 leading-sm">
-          Estreia: {formattedDate(movie?.release_date)}
-        </p>
-        <p>
-          <span className="flex">
-            <svg
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              className="h-5 text-colors-amber-400"
-            >
-              <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-            </svg>
-            {movie.vote_average}
-          </span>
-        </p>
+      <div className="cardMovieInfo--section">
+        <div className="cardMovieInfo--playSection">
+          <div className="cardMovieInfo--playSectionButton">
+            <ReactSVG width={40} src="/assets/svg/play-circle-regular.svg" />
+            Reproduzir
+          </div>
+          <div className="cardMovieInfo--playSectionActions">
+            <ReactSVG width={25} src="/assets/svg/plus-solid.svg" />
+            <ReactSVG width={25} src="/assets/svg/ban-solid.svg" />
+          </div>
+        </div>
+        <div className="cardMovieInfo--Details">
+          <h3>Incluído no pacote</h3>
+          <h2>{movie.title}</h2>
+          <p>{movieOverview}</p>
+          <div className="cardMovieInfo--DetailsFooter">
+            <p>{formattedDate(movie?.release_date)}</p>
+            <p>
+              <span className="flex">
+                <svg
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  className="h-5 text-colors-amber-400"
+                >
+                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                </svg>
+                {movie.vote_average}
+              </span>
+            </p>
+            <ReactSVG width={18} src="/assets/svg/comment-alt-regular.svg" />
+          </div>
+        </div>
       </div>
-      <button
-        onClick={() => history.push(`/movie/${movie.id}`)}
-        className="hover:bg-primary-600 bg-gradient-to-r from-primary-900 bg-colors-teal-700 uppercase tracking-widest w-full flex justify-center py-2 text-colors-white font-semibold transition duration-300 "
-      >
-        Sobre
-      </button>
-    </div>
+    </CardMovieInfo>
   );
 };
 
---
+++ src/components/CardMovie/styles.js
@@ -0,0 +1,88 @@
+import styled, { css } from "styled-components";
+
+export const CardMovieInfo = styled.div`
+  width: 20rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  background-color: #1b2530;
+  border-radius: 8px;
+  // overflow: hidden;
+  img {
+    border-radius: 8px;
+    cursor: pointer;
+  }
+  &:hover {
+    transform: scale(1.1);
+    transition: ease all 0.5s;
+    border: 2px solid #19b8da;
+    .cardMovieInfo--section {
+      display: flex;
+      margin-top: 172px;
+      border: 2px solid #19b8da;
+    }
+  }
+  .cardMovieInfo--section {
+    position: fixed;
+    width: inherit;
+    flex-direction: column;
+    display: none;
+    background: linear-gradient(to top, #1b2530, #000);
+    align-items: center;
+    border-radius: 0 0 8px 8px;
+    .cardMovieInfo--playSection {
+      width: 85%;
+      display: flex;
+      justify-content: space-between;
+      color: #fff;
+      margin: 15px 0;
+      .cardMovieInfo--playSectionButton {
+        display: flex;
+        align-items: center;
+        font-weight: bold;
+        cursor: pointer;
+      }
+      .cardMovieInfo--playSectionActions {
+        color: #fff;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      svg {
+        margin-right: 10px;
+        cursor: pointer;
+      }
+    }
+    .cardMovieInfo--Details {
+      width: 85%;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      color: #fff;
+      text-align: left;
+      h3 {
+        color: #19b8da;
+      }
+      h2 {
+        font-size: 20px;
+        font-weight: bold;
+      }
+      p {
+        font-size: 13px;
+      }
+      .cardMovieInfo--DetailsFooter {
+        width: inherit;
+        display: flex;
+        align-items: left;
+        margin: 10px 0;
+        p {
+          margin-right: 15px;
+        }
+        svg {
+          cursor: pointer;
+        }
+      }
+    }
+  }
+`;
